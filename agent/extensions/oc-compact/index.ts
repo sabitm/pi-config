12019fd50524a316628acf5c6e6d8c838cf922af
sabitm <sabitmaulanaa@gmail.com>
2026-08-16 08:14:01 +0700

    Remove oc-compact extension

--- agent/extensions/oc-compact/index.ts
+++
@@ -1,8 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-import { scaffoldConfig } from "./src/config";
-import { registerOcCompactHooks } from "./src/hooks";
-
-export default (pi: ExtensionAPI) => {
-  scaffoldConfig();
-  registerOcCompactHooks(pi);
-};
